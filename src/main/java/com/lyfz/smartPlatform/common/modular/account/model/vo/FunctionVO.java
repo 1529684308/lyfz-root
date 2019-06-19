1b842680235d3a58dce7c5ad30e2045ebd82ed9f
lxl <lee_xiao_liang@163.com>
2019-06-19 14:26:30 +0800

    功能VO

---
+++ src/main/java/com/lyfz/smartPlatform/common/modular/account/model/vo/FunctionVO.java
@@ -0,0 +1,32 @@
+package com.lyfz.smartPlatform.common.modular.account.model.vo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class FunctionVO {
+
+    private Integer enterpriseId;
+
+    private Integer shopId;
+
+    private String moduleFunId;
+
+    private String parentId;
+
+    private String moduleId;
+
+    private String name;
+
+    private Boolean menu;
+
+    private Integer sort;
+
+    private String ico;
+
+    private String url;
+
+    private Date end_time;
+}
+
