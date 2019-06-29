824115a978f16502b5a3e0347bf1a72772d72883
mzh <nicegood7771@163.com>
2019-06-29 10:36:45 +0800

    增加通用模块的订单服务传输给客户服务的DTO，修改了pom文件加了Core包

---
+++ src/main/java/com/lyfz/smartPlatform/common/modular/customer/model/dto/RemoveOrderCustomerDto.java
@@ -0,0 +1,16 @@
+package com.lyfz.smartPlatform.common.modular.customer.model.dto;
+
+import com.lyfz.smartPlatform.core.model.dto.IDto;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @Author: mzh
+ * @CreateTime: 2019-06-28
+ */
+@Data
+public class RemoveOrderCustomerDto implements IDto {
+    private List<Long> babyIds;
+    private List<Long> contactIds;
+}
