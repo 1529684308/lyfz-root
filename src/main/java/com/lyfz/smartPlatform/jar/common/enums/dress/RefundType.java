fe7d7776237726b116b1105ee7dd611800a2fad9
lxl <lee_xiao_liang@163.com>
2019-12-20 17:05:20 +0800

    添加枚举

--- src/main/java/com/lyfz/smartPlatform/jar/common/enums/dress/RefundType.java
+++ src/main/java/com/lyfz/smartPlatform/jar/common/enums/dress/RefundType.java
@@ -2,22 +2,15 @@ package com.lyfz.smartPlatform.jar.common.enums.dress;
 
 import com.baomidou.mybatisplus.core.enums.IEnum;
 
-public enum OrderStatusEnum implements IEnum<String> {
-
-    WAIT_CHOOSE("待选衣", "WAIT_CHOOSE"),
-    CHOOSE("已选衣", "CHOOSE"),
-    PACK("已整件", "PACK"),
-    FETCH("已取衣", "FETCH"),
-    BACK("已回衣", "BACK"),
-    NOT_BACK("未回衣", "NOT_BACK"),
-    SOME_BACK("部分回衣", "SOME_BACK"),
-    CANCEL("订单取消", "CANCEL"),
-    ;
+public enum RefundType implements IEnum<String> {
+    DEPOSIT("押金", "DEPOSIT"),
+    BROKEN("报损", "BROKEN")
+            ;
     private String desc;
     private String message;
 
 
-    OrderStatusEnum(String desc, String message) {
+    RefundType(String desc, String message) {
         this.desc = desc;
         this.message = message;
     }
@@ -34,5 +27,5 @@ public enum OrderStatusEnum implements IEnum<String> {
     @Override
     public String getValue() {
         return message;
-    } {}
+    }
 }
